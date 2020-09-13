43e3f42f4cf9a4ab8ce85b98dfab47dc4e2d0559
TheBoogle <theboogledoo@gmail.com>
2020-09-13 12:28:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import random
 import asyncio
 import aiohttp
 import os
-import mysql
-import mysql.connector
 
 Bot = discord.Client(shard_count=1)
 bot = commands.Bot(command_prefix='$')
@@ -28,17 +26,13 @@ async def update_status():
 		await bot.change_presence(status=discord.Status.idle, activity=discord.Game(statuses[status]))
 		await asyncio.sleep(10)
 		
-		
-		
-# ~ bot.remove_command('help')
 @bot.event
 async def on_ready():
 	print('\033[92m{0.user} is online.'.format(bot)+"\033[0m")
 	# ~ print('\033[92mConnected to proxy:{0.proxy}.'.format(Bot)+"\033[0m")
 	print('\033[92m{0.shard_count} shards running\033[0m'.format(Bot))
 	bot.loop.create_task(update_status())
-def generateXP():
-	return random.randint(1,max_XP_per_msg)
+
 			
 @bot.command()
 async def testcommand (ctx):
@@ -50,32 +44,7 @@ async def testcommand (ctx):
 		role = await guild.create_role(name='Member', permissions=perms, reason="")
 		await ctx.message.author.add_roles(role)
 
-@bot.command()
-@commands.is_owner()
-async def setlevel(ctx, member:discord.User=None, level: int=None):
-	cursor = mydb.cursor()
-	try:
-		cursor.execute("UPDATE users SET user_xp = "+ str(xp_per_level*level)+" WHERE client_id = " +str(member.id))
-		await ctx.send("Set "+member.mention+"'s level to `"+str(level)+"`")
-	except:
-		embed=discord.Embed(color=0xf00a3a)
-		embed.add_field(name="Error!", value="Either level is too high, or user is not in database", inline=True)
 
-		await ctx.send(embed=embed)
-	mydb.commit()
-# ~ @bot.command()
-# ~ @commands.is_owner()
-# ~ async def resetlevels(ctx):
-	# ~ cursor = mydb.cursor()
-	# ~ cursor.execute("SELECT client_id from users ORDER BY user_xp DESC")
-	# ~ result2 = cursor.fetchall()
-	# ~ i=0
-	# ~ while i < len(result2):
-		# ~ cursor.execute("DELETE FROM users WHERE client_id = "+str(result2[i][0]))
-		# ~ i=i+1
-	# ~ await ctx.send("Reset EVERYONES level!")
-	# ~ mydb.commit()
-@bot.command()
 @commands.has_permissions(manage_nicknames=True)
 async def resetnicknames(ctx):
 	members = ctx.guild.members
