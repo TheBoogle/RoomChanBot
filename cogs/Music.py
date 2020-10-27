c0edc7d455fff446c43481ed1b13f70d870f1808
TheBoogle <theboogledoo@gmail.com>
2020-10-27 12:10:00 -0700

    z

--- cogs/Music.py
+++ cogs/Music.py
@@ -6,12 +6,21 @@ import asyncio
 import aiohttp
 import os
 
-class Tools(commands.Cog):
 
+class Music(commands.Cog):
     def __init__(self, bot):
-		self.bot = bot
+        self.bot = bot
         
     print("\033[92mLoading Music Cog\033[0m")
+    
+   
+    @commands.command(help='Begins RoomChan Radio Playback')
+    async def radio(self, ctx):
+        radio_channel = self.bot.get_channel(770719723851612170)
+        vc = await radio_channel.connect()
+        
+        vc.play(discord.FFmpegPCMAudio("test.mp3"))
+        
         
 def setup(bot):
-	bot.add_cog(Tools(bot))
+    bot.add_cog(Music(bot))
