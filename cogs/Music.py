f26a598a30afbb52348f9682cd113f614851f402
TheBoogle <theboogledoo@gmail.com>
2020-10-27 14:36:42 -0700

    Large update

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -6,20 +7,75 @@ import asyncio
 import aiohttp
 import os
 
+import youtube_dl
+import soundfile as sf
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '1'
+    }],
+    'prefer_ffmpeg': True,
+    'keepvideo': False
+}
+
+vc = None
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
     print("\033[92mLoading Music Cog\033[0m")
     
+    @commands.command(help='Ends RoomChan Radio Playback')
+    async def disconnect(self, ctx):
+        await vc.disconnect()
    
+
     @commands.command(help='Begins RoomChan Radio Playback')
-    async def radio(self, ctx):
+    async def radio(self, ctx, url):
+        if not url:
+            await ctx.channel.send('No URL provided')
+            return
         radio_channel = self.bot.get_channel(770719723851612170)
-        vc = await radio_channel.connect()
         
-        vc.play(discord.FFmpegPCMAudio("test.mp3"))
+        try:    
+            vc = await radio_channel.connect()
+        except:
+            await ctx.channel.send("A song is already playing")
+            return
+        
+        for file in os.listdir("./"):
+            if file.endswith(".wav"):
+                os.remove(file)
+        
+        await ctx.channel.send('Downloading & Converting file... This will take a few seconds.')
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+        except:
+            await ctx.channel.send("This song can't be played due to DMCA")
+            await vc.disconnect()
+            return
+        
+        for file in os.listdir("./"):
+            if file.endswith(".wav"):
+                os.rename(file, 'song.wav')
+        try:
+            vc.play(discord.FFmpegPCMAudio("song.wav"))
+            await ctx.channel.send('Playing.')
+           
+            f = sf.SoundFile('song.wav')
+            print(len(f) / f.samplerate)
+            await asyncio.sleep(len(f) / f.samplerate)
+            await vc.disconnect()
+            
+        except:
+            await ctx.channel.send("A song is already playing")
+        
+        
+        
         
         
 def setup(bot):
