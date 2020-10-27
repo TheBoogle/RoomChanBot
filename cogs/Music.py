492b610402398a30a0f5f76d62d3c60a0d6494eb
TheBoogle <theboogledoo@gmail.com>
2020-10-27 14:06:35 -0700

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -5,6 +6,19 @@ import random
 import asyncio
 import aiohttp
 import os
+
+import youtube_dl
+
+
+ydl_opts = {
+        'format': 'bestaudio/best',
+        'external-downloader':'aria2',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '1024',
+        }],
+    }
 
 
 class Music(commands.Cog):
@@ -17,9 +31,16 @@ class Music(commands.Cog):
     @commands.command(help='Begins RoomChan Radio Playback')
     async def radio(self, ctx):
         radio_channel = self.bot.get_channel(770719723851612170)
-        vc = await radio_channel.connect()
-        
-        vc.play(discord.FFmpegPCMAudio("test.mp3"))
+        try:    
+            vc = await radio_channel.connect()
+        except:
+            pass
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(['https://www.youtube.com/watch?v=6NTfCbfvwM8'])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, 'song.mp3')
+        vc.play(discord.FFmpegPCMAudio("song.mp3"))
         
         
 def setup(bot):
