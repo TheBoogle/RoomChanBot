b1974a58e5f442e33fed6ca49f03dcdb3716c956
TheBoogle <theboogledoo@gmail.com>
2021-01-02 21:11:28 -0800

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -20,12 +20,12 @@ bannedFileTypes = ['exe', 'dll', 'bat', 'zip', 'rar', 'img', 'iso', '7z','pdf', 
 
 statuses = ["smh my head"]
 
-# mydb = mysql.connector.connect(
-# 	host="localhost",
-# 	user="boog",
-# 	passwd="lol",
-# 	database="RoomChan"
-# )
+mydb = mysql.connector.connect(
+	host="localhost",
+	user="boog",
+	passwd="lol",
+	database="RoomChan"
+)
 
 Bot = discord.Client()
 bot = commands.Bot(command_prefix='$', shard_count = 1, intents = discord.Intents.all(), chunk_guilds_at_startup=True)
